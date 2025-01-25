1f7c4e9be14389516ab43f938e9aaa0e61b58939
Kauã Souza <kaua.jesuscristo78@gmail.com>
2025-01-25 19:10:28 -0300

    funcionando

--- src/main/java/com/mycompany/mvpinclusaoproduto/view/PrincipalProdutoView.java
+++ src/main/java/com/mycompany/mvpinclusaoproduto/view/PrincipalProdutoView.java
@@ -4,21 +4,19 @@
  */
 package com.mycompany.mvpinclusaoproduto.view;
 
-import javax.swing.JLabel;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
+import javax.swing.JButton;
+import javax.swing.JTable;
 
 /**
  *
  * @author kauac
  */
-public class PrincipalProdutoView extends javax.swing.JFrame {
+public class ListagemProdutoView extends javax.swing.JFrame {
 
     /**
-     * Creates new form PrincipalProdutoView
+     * Creates new form ListagemProdutoView
      */
-    public PrincipalProdutoView() {
+    public ListagemProdutoView() {
         initComponents();
         setLocationRelativeTo(null);
     }
@@ -32,111 +30,75 @@ public class PrincipalProdutoView extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
-        lblQuantProdutosCadastrados = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
-        jLabel4 = new javax.swing.JLabel();
-        jMenuBar = new javax.swing.JMenuBar();
-        menuGerenciarProdutos = new javax.swing.JMenu();
-        menuIncluirProduto = new javax.swing.JMenuItem();
-        menuListarProduto = new javax.swing.JMenuItem();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        tableProdutos = new javax.swing.JTable();
+        btnRemover = new javax.swing.JButton();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setTitle("Home");
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setTitle("Listagem de Produto");
 
-        jLabel1.setText("Total de Produtos Cadastrados:");
+        tableProdutos.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
 
-        lblQuantProdutosCadastrados.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+            },
+            new String [] {
+                "id", "nome", "percentual de lucro (%)", "preço de custo", "valor de venda"
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.Integer.class, java.lang.String.class, java.lang.Double.class, java.lang.Double.class, java.lang.Double.class
+            };
 
-        jLabel2.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
-        jLabel2.setText("Sistema de Produtos");
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+        });
+        jScrollPane1.setViewportView(tableProdutos);
+        if (tableProdutos.getColumnModel().getColumnCount() > 0) {
+            tableProdutos.getColumnModel().getColumn(0).setMaxWidth(30);
+        }
 
-        jLabel3.setText("Desenvolvido por Kauã Silva");
-
-        jLabel4.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
-
-        menuGerenciarProdutos.setText("Gerenciar Produtos");
-
-        menuIncluirProduto.setText("Incluir produto");
-        menuGerenciarProdutos.add(menuIncluirProduto);
-
-        menuListarProduto.setText("Listar produto");
-        menuGerenciarProdutos.add(menuListarProduto);
-
-        jMenuBar.add(menuGerenciarProdutos);
-
-        setJMenuBar(jMenuBar);
+        btnRemover.setText("Remover");
+        btnRemover.setEnabled(false);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap(129, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addComponent(jLabel2)
-                        .addGap(121, 121, 121))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addComponent(jLabel3)
-                        .addGap(163, 163, 163))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addComponent(jLabel1)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(lblQuantProdutosCadastrados, javax.swing.GroupLayout.PREFERRED_SIZE, 46, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(27, 27, 27))))
-            .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 666, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(btnRemover)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(36, 36, 36)
-                .addComponent(jLabel2)
-                .addGap(18, 18, 18)
-                .addComponent(jLabel3)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 177, Short.MAX_VALUE)
-                .addComponent(jLabel4)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(lblQuantProdutosCadastrados, javax.swing.GroupLayout.PREFERRED_SIZE, 16, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel1))
-                .addContainerGap())
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 281, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(129, 129, 129)
+                        .addComponent(btnRemover)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JMenuBar jMenuBar;
-    private javax.swing.JLabel lblQuantProdutosCadastrados;
-    private javax.swing.JMenu menuGerenciarProdutos;
-    private javax.swing.JMenuItem menuIncluirProduto;
-    private javax.swing.JMenuItem menuListarProduto;
+    private javax.swing.JButton btnRemover;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable tableProdutos;
     // End of variables declaration//GEN-END:variables
 
-    public JMenuBar getjMenuBar() {
-        return jMenuBar;
+    public JTable getTableProdutos() {
+        return tableProdutos;
     }
 
-    public JMenu getMenuGerenciarProdutos() {
-        return menuGerenciarProdutos;
+    public JButton getBtnRemover() {
+        return btnRemover;
     }
-
-    public JMenuItem getMenuIncluirProduto() {
-        return menuIncluirProduto;
-    }
-
-    public JMenuItem getMenuListarProduto() {
-        return menuListarProduto;
-    }
-
-    public JLabel getLblQuantProdutosCadastrados() {
-        return lblQuantProdutosCadastrados;
-    }
-    
 }
